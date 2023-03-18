9c60bdba820eaaab72a8748d3ea2189f3cc9fe9e
RENaa_FD <92320175+lRENyaaa@users.noreply.github.com>
2023-03-18 23:00:32 +0800

    1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -78,7 +78,7 @@ gradleEnterprise {
 
 include("mappings:shared")
 setOf("1-18", "1-18-2", "1-19", "1-19-3", "1-19-4").forEach {
-    include("mappings:mc-$it")
+    include("mappings:mc-$it" )
 }
 
 setOf("shared", "propertyold", "propertynew").forEach {
